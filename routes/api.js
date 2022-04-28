2690ba4cc01c7981dc045fc6b11a33af787f70c6
Shallom <micahshallom@gmail.com>
2022-04-28 23:00:11 +0100

    added_more_screenshots

--- routes/api.js
+++ routes/api.js
@@ -2,13 +2,13 @@ const express = require('express')
 const router = express.Router();
 const Todo = require('../models/todo')
 
-router.get('/todo', (req, res, next) => {
+router.get('/todos', (req, res, next) => {
     Todo.find({}, 'action')
     .then(data => res.json(data))
     .catch(next)
 })
 
-router.post('/todo', (req, res,next) => {
+router.post('/todos', (req, res,next) => {
     if(req.body.action){
         Todo.create(req.body)
         .then(data => res.json(data))
@@ -18,7 +18,7 @@ router.post('/todo', (req, res,next) => {
     }
 })
 
-router.delete('/todo:id', (req, res) => {
+router.delete('/todos:id', (req, res) => {
     Todo.findOneAndDelete({_id:req.params.id})
     .then(data => req.json(data))
     .catch(next)
